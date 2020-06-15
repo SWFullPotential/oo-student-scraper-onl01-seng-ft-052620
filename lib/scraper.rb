7eed8c4920cc671992e53550de3e3d226a0467b4
Samantha Wabi <swfullpotential@gmail.com>
2020-06-15 03:42:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,39 +4,27 @@ require 'pry'
 
 class Scraper
 
+ require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+class Scraper
+  
   def self.scrape_index_page(index_url)
-      students_hash = []
-    html = Nokogiri::HTML(open(index_url))
-    html.css(".student-card").collect do |student|
-      hash = {
-        name: student.css("h4.student-name").text,
-        location: student.css("p.student-location").text,
-        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+    
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value 
+      student_info = {
+        :name => name, 
+        :location => location, 
+        :profile_url => profile_url
       }
-      students_hash << hash
+      students << student_info
     end
-    students_hash
-    # binding.pry
+    students
   end
   
-  
-
-def self.scrape_profile_page(profile_url)
-   student = {} 
-   profile_page = Nokogiri::HTML(open(profile_url))
-   links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attributes('href').value}
-   links.each do |link|
-     if link.include?("linkedin")
-       student[:linkedin] = link
-       elsif link.include?("github")
-       student[:github] = link
-       elsif link.include?("twitter")
-       student[:twitter] = link 
-     else
-       student[:blog] = link
-     end
-   end
-  student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-  student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-end
-end
