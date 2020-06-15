945c5d1ce778b38fc54b16ad90248745e4aa1c21
Samantha Wabi <swfullpotential@gmail.com>
2020-06-15 03:47:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,6 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-
- require 'open-uri'
-require 'nokogiri'
-require 'pry'
-
-class Scraper
   
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
